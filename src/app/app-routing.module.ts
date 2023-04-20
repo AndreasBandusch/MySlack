73ea4c35111be51f8118249155c31ee0712d3805
Andreas Bandusch <andreas.bandusch@gmx.de>
2023-04-20 14:52:20 +0200

    Added intial components and fonts. Edited header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { SendEmailComponent } from './components/send-email/send-email.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LogInComponent, pathMatch: 'full', },
+  { path: 'login', component: LogInComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'send-email', component: SendEmailComponent },
+  { path: 'app', component: MainContentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,28 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SendEmailComponent } from './components/send-email/send-email.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { HeaderComponent } from './components/main-content/header/header.component';
+import { MenuComponent } from './components/main-content/menu/menu.component';
+import { MainChatComponent } from './components/main-content/main-chat/main-chat.component';
+import { ThreadComponent } from './components/main-content/thread/thread.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    SignInComponent,
+    SendEmailComponent,
+    ResetPasswordComponent,
+    MainContentComponent,
+    HeaderComponent,
+    MenuComponent,
+    MainChatComponent,
+    ThreadComponent
   ],
   imports: [
     BrowserModule,
