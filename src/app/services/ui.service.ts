fb48cd50427576fecf7ffd60da6ef41acc27335b
Andreas Bandusch <andreas.bandusch@gmx.de>
2023-04-30 15:02:20 +0200

    Designed channels menu

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -7,6 +7,7 @@ import { Injectable } from '@angular/core';
 export class UiService {
   showMenu = true;
   showThread: boolean = true;
+  activeLink: string = '';
   overlays: any = {
     userMenu: false,
     profile: false,
