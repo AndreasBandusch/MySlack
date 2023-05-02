ec88a17265be2efde5b548f0ed326c091266b366
Andreas Bandusch <andreas.bandusch@gmx.de>
2023-05-02 16:12:47 +0200

    Added online status feature

--- src/app/components/main-content/menu/direct-messages/direct-messages.component.ts
+++ src/app/components/main-content/menu/direct-messages/direct-messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
@@ -9,14 +9,20 @@ import { Observable } from 'rxjs';
   templateUrl: './direct-messages.component.html',
   styleUrls: ['./direct-messages.component.scss']
 })
-export class DirectMessagesComponent {
+export class DirectMessagesComponent implements OnInit {
   firestore: Firestore = inject(Firestore)
   items$: Observable<any[]>;
 
   menuIsOpen: boolean = true;
+
   constructor(public ui: UiService) { 
     const aCollection = collection(this.firestore, 'user')
     this.items$ = collectionData(aCollection);
+  }
+
+
+  ngOnInit(): void {
+    
   }
 
   toogleChannels() {
